341b58773ffd91024b63b7be0c72ccc678b19fb7
juliema <juliema@nrb-dhcp-24.inhs.illinois.edu>
2014-07-28 12:50:00 -0500

    NewCollectorData Scripts

---
+++ CollectorData/getcollectorsall.pl
@@ -0,0 +1,44 @@
+#!usr/bin/env perl
+
+use strict;
+use warnings;
+
+my %transcriberhash=();
+my @transcriberarray=();
+my $total=0;
+my $totaltranscriptions=0;
+
+system "ls -l *.txt >filenames";
+
+open OUT, ">Master.Transcritber.info.txt";
+open FH, "<filenames"; 
+while (<FH>) {
+    if (/(\S+).CollectorData.txt/) {
+	my $collection=$1;
+	print "$collection";
+	open FH1, "<$collection.CollectorData.txt";
+	while (<FH1>) {
+	    if (/(.*)\s+(\d+)$/) {
+		my $transcriber=$1;
+		my $numtranscriptions=$2;
+		$totaltranscriptions=$totaltranscriptions+$numtranscriptions;
+		if (! exists $transcriberhash{$transcriber}) {
+		    $transcriberhash{$transcriber}=$numtranscriptions;
+		    push @transcriberarray, $transcriber;
+		    $total++;
+		}
+		if ( exists $transcriberhash{$transcriber}) {
+#		    print "$transcriber\t$transcriberhash{$transcriber}\t$numtranscriptions\t";
+		    $transcriberhash{$transcriber}=$transcriberhash{$transcriber}+$numtranscriptions;
+#		    print "$transcriberhash{$transcriber}\n";
+		}
+	    }
+	}
+    }
+}
+
+for my $transcriber(@transcriberarray) {
+    print OUT "$transcriber\t$transcriberhash{$transcriber}\n";
+}
+
+print "There are $total transcribers and $totaltranscriptions transcriptions\n";
---
+++ CollectorData/transcriptioncount.pl
@@ -0,0 +1,42 @@
+#!usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $upperlimit=10;
+
+my %transcriptionhash=();
+my @transcriptionarray=();
+my $total=0;
+my $totaltranscriptions=0;
+
+
+open FH, "<Master.Transcritber.info.txt";
+while (<FH>) {
+    if (/.*\s+(\d+)/) {
+	my $numtranscriptions=$1;
+	$totaltranscriptions=$totaltranscriptions+$numtranscriptions;
+	if (! exists $transcriptionhash{$numtranscriptions}) { $transcriptionhash{$numtranscriptions}=1; push @transcriptionarray, $numtranscriptions; }
+	elsif (exists $transcriptionhash{$numtranscriptions}) { $transcriptionhash{$numtranscriptions}++; }
+    }
+}
+
+open OUT, ">TranscribersperNumberofTranscriptions.txt";
+print OUT "NumTranscriptions\tNumTranscribers\n";
+
+for (1..$upperlimit) {
+    my $num=$_;
+
+    if ($transcriptionhash{$num} != "") {
+	print OUT "$num\t$transcriptionhash{$num}\n";
+	print  "$num\t$transcriptionhash{$num}\n";
+	$total=$total+$transcriptionhash{$num};
+    }
+    else {
+	print OUT "$num\t0\n";
+	print  "$num\t0\n";
+    }
+}
+
+print "$total in $upperlimit\n";
+print "$totaltranscriptions\n";
